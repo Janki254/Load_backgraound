2ee25522c31be4da51a868a2c3f2ceef5c0a70e3
Janki <janki.gohil@odaninfotech.com>
2023-04-03 23:20:32 +0530

    Adding code

---
+++ blur_effect.js
@@ -0,0 +1,25 @@
+const loadText = document.querySelector('.loading-text');
+const bg = document.querySelector('.bg');
+const btn = document.getElementById('blur_start');
+let load = 0;
+let int;
+btn.addEventListener('click', () => {
+    int = setInterval(blurring, 40);
+});
+function blurring() {
+    load++;
+
+    if (load > 99) {
+        clearInterval(int);
+    }
+
+    loadText.innerText = `${load}%`;
+    loadText.style.opacity = scale(load, 0, 100, 1, 0);
+    bg.style.background = `url('./assets/night-sky.jpg') no-repeat center center/cover`;
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+    btn.style.display = 'none';
+}
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
